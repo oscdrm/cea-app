484cc34f17da304aac2ba1ffae85b41202224760
Oscar Daniel Rangel Martinez <oscar.rangel@lunave.com>
2021-04-13 22:55:41 -0500

    testing

--- resources/views/movimientos/show.blade.php
+++ resources/views/movimientos/show.blade.php
@@ -130,6 +130,16 @@
                             <figure>
                             <a href="{{asset($movimiento->comprobante_pago)}}" title="{{asset($movimiento->folio)}}" data-gallery=""><img src="{{asset($movimiento->comprobante_pago)}}"></a>
                             </figure>
+                            <!-- The Gallery as lightbox dialog, should be a child element of the document body -->
+                            <div id="blueimp-gallery" class="blueimp-gallery">
+                                <div class="slides"></div>
+                                <h3 class="title"></h3>
+                                <a class="prev">‹</a>
+                                <a class="next">›</a>
+                                <a class="close">×</a>
+                                <a class="play-pause"></a>
+                                <ol class="indicator"></ol>
+                            </div>
                         </div>
                     </div>
                 @endif
